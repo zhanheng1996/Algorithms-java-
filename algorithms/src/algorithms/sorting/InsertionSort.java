450c56f279919cf2e4f19019d0af2841c79ead61
zhanheng <444931488@qq.com>
2018-08-29 17:23:31 +0800

    insertion sort update

--- algorithms/src/algorithms/sorting/InsertionSort.java
+++ algorithms/src/algorithms/sorting/InsertionSort.java
@@ -7,6 +7,7 @@ public class InsertionSort {
         Scanner sc = new Scanner(System.in);
         System.out.print("请输入数组大小：");
         int cur;
+        int j;
         int n = sc.nextInt();
         int[] array = new int[n];
         for (int m = 0; m < n; m++) {
@@ -14,10 +15,11 @@ public class InsertionSort {
         }
         //插入排序
         for (int i = 1; i < n; i++) {
-            cur = a[i];
-            for (int j = i - 1; j >= 0 && a[j] > cur; j--)
-                a[j + 1] = a[j];
-            a[j + 1] = cur;
+            cur = array[i];
+            for (j = i - 1; j >= 0 && array[j] > cur; j--){
+                array[j + 1] = array[j];
+            }
+            array[j + 1] = cur;
         }
         for (int i = 0; i < n; i++) {
             System.out.print(array[i] + " ");
