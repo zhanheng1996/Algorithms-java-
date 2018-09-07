d65cff93a71af28f00bee631b3cc66184c5f07f0
zhanheng <444931488@qq.com>
2018-09-07 17:28:02 +0800

    Reverse Linked List

---
+++ algorithms/src/algorithms/lintcode/reverselinkedlist/ReverseLinkedList.java
@@ -0,0 +1,24 @@
+package algorithms.lintcode.reverselinkedlist;
+
+public class ReverseLinkedList {
+
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+        return prev;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
